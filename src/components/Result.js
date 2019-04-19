45532f823c4dab978743b705c1bc42b29851d924
Rubiksron <rubiksron@gmail.com>
2019-04-19 10:51:34 -0700

    created a new repo for this project because the last repo was broken.

---
+++ src/components/Result.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CSSTransitionGroup } from 'react-transition-group';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+function Result(props) {
+
+  return (
+    <ReactCSSTransitionGroup
+      className="container result"
+      component="div"
+      transitionName="fade"
+      transitionEnterTimeout={800}
+      transitionLeaveTimeout={500}
+      transitionAppear
+      transitionAppearTimeout={500}
+    >
+      <div>
+        <h2 className="center">You got {props.answersCount.correct} right and {props.answersCount.wrong} wrong, well done!</h2>
+      </div>
+    </ReactCSSTransitionGroup>
+  );
+}
+
+Result.propTypes = {
+  answersCount: PropTypes.object.isRequired
+};
+
+export default Result;
